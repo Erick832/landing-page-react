b98854891350e84a6fc7250a42f2f13349441a57
Erick <erick19arones@gmail.com>
2022-11-15 16:31:24 -0500

    Add header and footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../images/logo-tech.png";
+function Navbar() {
+  return (
+    <nav className="nav-link">
+      <a href="#id-home">
+        <img className="img-logo" src={logo} alt="logo tech academy" />
+      </a>
+      <div className="div-search">
+        <i id="icon-search" className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Buscar" />
+      </div>
+      <button className="button-header">
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      <ul>
+        <li>
+          <a href="#id-objetivos">Objetivos</a>
+        </li>
+        <li>
+          <a href="#id-beneficios">Beneficios</a>
+        </li>
+        <li>
+          <a href="#id-miembros">Miembros</a>
+        </li>
+        <li>
+          <a href="#id-contactos">Contactos</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
